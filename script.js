18b5c1baa0bdb317e232b501f3683cf6fa9e1409
imhereforfuniguess <106017717+imhereforfuniguess@users.noreply.github.com>
2022-12-01 22:13:36 +0200

    Add basic html start js

---
+++ script.js
@@ -0,0 +1,89 @@
+let num1 = 5;
+let num2 = 8;
+let numAccumulator = "" 
+let actionSetter = ''
+
+function add(x,y){
+    return x + y;
+}
+
+function subtracts(x,y){
+    return x - y;
+}
+
+function multiply(x,y){
+    return x + y
+}
+
+function divide(x,y){
+    return x + y
+}
+
+function operate(caller, x, y){
+    caller(x,y)
+}
+
+
+const numberDisplay = document.querySelector('.display')
+const clearButton = document.querySelector('.clear')
+const equalsButton = document.querySelector('.equals')
+const actionButton = document.querySelectorAll('.action')
+const numpad = document.querySelectorAll('.numpad')
+
+
+equalsButton.addEventListener("click", () => {
+    num2 = parseInt(numAccumulator);
+    numAccumulator = ""
+    console.log(num2)
+    numberDisplay.textContent = numberDisplay.textContent + equalsButton.textContent
+
+    result = operate(actionSetter,num1,num2);
+    numberDisplay.textContent = numberDisplay.textContent + result
+    }
+)
+
+
+actionButton.forEach(item => 
+    item.addEventListener("click", () => {
+        num1 = parseInt(numAccumulator);
+        numAccumulator = ""
+        console.log(num1)
+        numberDisplay.textContent = numberDisplay.textContent + item.textContent
+        
+        // set action to 
+        switch (item.textContent){
+            case '+':
+                actionSetter = "sum"
+                break;
+            case '-':
+                actionSetter = "sub"
+                break;
+            case '*':
+                actionSetter = "mul"
+                break;
+                
+            case '/':
+                actionSetter = "div"
+                break;
+        }
+    })
+)
+
+
+numpad.forEach(item => 
+    item.addEventListener('click', event => {
+        numAccumulator = numAccumulator + item.textContent
+        console.log(numAccumulator)
+        numberDisplay.textContent = numberDisplay.textContent + item.textContent
+    })
+)
+
+clearButton.addEventListener('click', event => {
+    numberDisplay.textContent = ""
+    numAccumulator = ""
+})
+
+
+// Function button pressed -> 
+// 1) Accumulator put in memory as int and set to 0
+// 2) 
