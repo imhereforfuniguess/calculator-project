193b7e527ad05f6c9cadc9d3c6745f8a8e409f5c
imhereforfuniguess <106017717+imhereforfuniguess@users.noreply.github.com>
2022-12-01 23:14:15 +0200

    Move equals function outside of = button

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let num1 = 5;
-let num2 = 8;
+let num1 = 0
+let num2 = 0
 let numAccumulator = "" 
-let actionSetter
+let actionSetter = "mul"
+let secondActionFlag = 0
+let result = 0
 
+
+//operate (actionSetter,num1,num2);
 
 function add(x,y){
     console.log(x+y);
@@ -36,40 +40,43 @@ const actionButton = document.querySelectorAll('.action')
 const numpad = document.querySelectorAll('.numpad')
 
 
-equalsButton.addEventListener("click", () => {
-    num2 = parseInt(numAccumulator);
-    numAccumulator = ""
+equalsButton.addEventListener("click", pressEquals);
+
+function pressEquals(){
     numberDisplay.textContent = numberDisplay.textContent + equalsButton.textContent
-    let result = operate(actionSetter,num1,num2);
+    console.log(actionSetter)
+    result = operate (actionSetter, num1, parseInt(numAccumulator))
 
     numberDisplay.textContent = numberDisplay.textContent + result
-    }
-)
-
+    secondActionFlag = 0
+    numAccumulator = ""
+}
 
 actionButton.forEach(item => 
     item.addEventListener("click", () => {
-        num1 = parseInt(numAccumulator);
-        numAccumulator = ""
-        console.log(num1)
-        numberDisplay.textContent = numberDisplay.textContent + item.textContent
         
-        // set action to 
+        numberDisplay.textContent = numberDisplay.textContent + item.textContent
+        num1 = parseInt(numAccumulator);        
+        // switch based on action button pressed 
         switch (item.textContent){
             case '+':
-                actionSetter = "sum"
-                break;
+                actionSetter = "add"
+                break
             case '-':
                 actionSetter = "sub"
-                break;
+                break
             case '*':
                 actionSetter = "mul"
-                break;
+                break
                 
             case '/':
                 actionSetter = "div"
-                break;
+                break
         }
+
+
+        numAccumulator = ""
+        secondActionFlag = 1
     })
 )
 
@@ -77,14 +84,16 @@ actionButton.forEach(item =>
 numpad.forEach(item => 
     item.addEventListener('click', event => {
         numAccumulator = numAccumulator + item.textContent
-        console.log(numAccumulator)
+        // console.log(numAccumulator)
         numberDisplay.textContent = numberDisplay.textContent + item.textContent
+        console.log(`result is:${result}, num1:${num1} numAccumulator:${numAccumulator}`)
     })
 )
 
 clearButton.addEventListener('click', event => {
     numberDisplay.textContent = ""
     numAccumulator = ""
+    secondActionFlag = 0
 })
 
 
